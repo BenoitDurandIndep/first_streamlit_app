67a096b90c7c92ed9722afe608abe23a569ac5e7
BenoitDurandIndep <43718519+BenoitDurandIndep@users.noreply.github.com>
2023-10-11 00:04:28 +0200

    Update streamlit_app.py add first menu
    add first menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title("Nice New App for my Parents")
+st.title("Nice New App for the restaurant")
+
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
