385a06aa3745f7ba07a09b4faf735a32dc45009c
BenoitDurandIndep <43718519+BenoitDurandIndep@users.noreply.github.com>
2023-10-20 23:16:55 +0200

    Update streamlit_app.py add fruitivice tab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+import requests
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+AWS_FRUIT_MACROS="https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+URL_FRUITYVICE="https://fruityvice.com/api/fruit/"
+
+
+my_fruit_list = pd.read_csv(AWS_FRUIT_MACROS)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 st.title("Nice New App for the restaurant")
@@ -16,3 +21,13 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = st.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
+
+st.header("Fruityvice Fruit Advice!")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get(URL_FRUITYVICE+fruit_choice)
+
+# normalize json and print it
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+st.dataframe(fruityvice_normalized)
+
